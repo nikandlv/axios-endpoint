3eddf4f96d67d0a0b7cf4a0416af141ecc06f893
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-10 10:51:44 -0500

    Generate function now works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 "use strict";
 
 let paths = {
-    getAdmin: route('POST', '/Admin')
+    getAdmin: route('POST', '/Admin').generate()
 };
 
 function route(method,address) {
     data = {
-
+        method,
+        address
     }
     return {
         data,
@@ -16,9 +17,11 @@ function route(method,address) {
         after: () => {
 
         },
-        generate: () => {
-
-        }
+        generate: () => (data) =>  axios({
+                    method: data.method,
+                    url: data.address,
+                    data: data
+                })
     }
 }
 
