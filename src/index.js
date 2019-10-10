e83994e1d42c35441be367bb2d599016a099de22
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-10 09:39:11 -0500

    Generate now works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 "use strict";
 
+let paths = {};
+
 function makeEndpoint(paths) {
 
 }
 
 function generate() {
-
+    let endpoint = {}
+    Object.keys(paths).forEach(key => {
+        endpoint[key] = paths[key]
+    });
+    return endpoint;
 }
 
 module.exports = {
