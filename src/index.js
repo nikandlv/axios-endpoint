061d75900d1e1fdc97cdca7c4bfe79903585865a
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-13 18:28:14 +0330

    Moved everything to a class based solution

--- src/index.js
+++ src/index.js
@@ -2,28 +2,43 @@
 
 const axios = require('axios')
 
+class Bundle {
+
+    route = {};
+
+    constructor(route) {
+        this.route = route;
+    }
+
+    before(callback){
+        this.beforeCallback = callback;
+        return this
+    }
+
+    after(callback){
+        this.afterCallback = callback;
+        return this
+    }
+
+    generate() {
+        const beforeCallback = this.beforeCallback || (() => {});
+        const afterCallback = this.afterCallback || (() => {});
+        return (data) =>  {
+            beforeCallback()
+            return axios({
+                method: this.route.method,
+                url: this.route.address,
+                data
+            }).then(_ => {
+                afterCallback()
+            })
+        }
+    }
+}
+
 module.exports = function route(method,address) {
-    let bundle = {
-        route: {
-            method,
-            address
-        },
-    };
-
-    bundle.before = (callback) => {
-        bundle.route.before = callback;
-        return bundle
-    },
-
-    bundle.after = (callback) => {
-        bundle.route.after = callback;
-        return bundle
-    },
-
-    bundle.generate = () => (data) =>  axios({
-        method: route.method,
-        url: route.address,
-        data
-    })
-    return bundle
+    return new Bundle({
+        method,
+        address
+    });
 }
