86778ea106b9bdc3e10cd1e78da86e02d6103537
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-13 18:16:16 +0330

    Added before and after handlers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@
 const axios = require('axios')
 
 module.exports = function route(method,address) {
-    let route = {
-        method,
-        address
-    }
     let bundle = {
-        route,
+        route: {
+            method,
+            address
+        },
     };
+
+    bundle.before = (callback) => {
+        bundle.route.before = callback;
+        return bundle
+    },
+
+    bundle.after = (callback) => {
+        bundle.route.after = callback;
+        return bundle
+    },
+
     bundle.generate = () => (data) =>  axios({
         method: route.method,
         url: route.address,
