bc8ce97dbc9ad20ce8c933c136f672737ba03a23
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-13 18:29:20 +0330

    Added test for before callback

--- test/index.js
+++ test/index.js
@@ -3,18 +3,34 @@ var route = require('../src/index')
 
 const axios = require('axios')
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+//axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.baseURL = 'http://zoomit.ir';
 
 let endpoint = {
-    getTodos: route('GET','/todos/1').generate()
+    getTodos: route('GET','/todos/1').generate(),
+    getTodosBefore: route('GET','/todos/1').before(() => {
+        console.log('ok gonna fetch now!')
+    }).generate()
+
 }
 
 describe('Route', () => {
-    describe('Simple Request', () => {
-        it('Should get todos with status code 200', () => {
+    describe('Simple request', () => {
+        it('Should get todos', () => {
             endpoint.getTodos().then(data => {
+                console.log('ok')
                 assert.equal(data.status, 200)
+            }).catch(error => {
+                console.log('failed')
             })
+        })
+    })
+    it('Should get todos with before callback', () => {
+        endpoint.getTodosBefore().then(data => {
+            console.log('ok')
+            assert.equal(data.status, 200)
+        }).catch(error => {
+            console.log('failed')
         })
     })
 })
